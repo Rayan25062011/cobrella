7086e2f0a484d1506ffcc9bd8819198cef04aede
Rayan25062011 <101386337+Rayan25062011@users.noreply.github.com>
2023-03-14 11:41:21 +0200

    Update cobrella.py

--- cobrella/cobrella.py
+++ cobrella/cobrella.py
@@ -2,7 +2,6 @@ import os
 import sys
 
 if sys.argv:
-   print(green + "[" + bold + " STARTING... \033[0m" + green + " ]")
    
    os.system("pip install vulture")
    os.system(f"vulture {sys.argv}")
